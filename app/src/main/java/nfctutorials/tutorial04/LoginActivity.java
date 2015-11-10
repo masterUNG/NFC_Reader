a4a07a119d4b57d30c2ccfb86900690a2ba13eab
masterUNG <phrombutr@gmail.com>
2015-11-10 09:41:48 +0700

    Start Day2

--- app/src/main/java/nfctutorials/tutorial04/LoginActivity.java
+++ app/src/main/java/nfctutorials/tutorial04/LoginActivity.java
@@ -9,5 +9,6 @@ public class LoginActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-}
+    }   // Main Method
+
+}   // Main Class
