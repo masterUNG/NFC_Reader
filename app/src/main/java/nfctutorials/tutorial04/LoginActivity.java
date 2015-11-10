41dff9266a80fc74d67747744835685ba40868b9
masterUNG <phrombutr@gmail.com>
2015-11-10 14:37:35 +0700

    Synchronize Successful

--- app/src/main/java/nfctutorials/tutorial04/LoginActivity.java
+++ app/src/main/java/nfctutorials/tutorial04/LoginActivity.java
@@ -1,8 +1,22 @@
 package nfctutorials.tutorial04;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class LoginActivity extends ActionBarActivity {
 
@@ -31,6 +45,106 @@ public class LoginActivity extends ActionBarActivity {
 
     private void synJSONtoSQLite() {
 
+        //Setup Policy
+        StrictMode.ThreadPolicy myPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(myPolicy);
+
+        int intTimes = 1;
+        while (intTimes <= 2) {
+
+            //1. Create InputStream
+            InputStream objInputStream = null;
+            String strJSON = null;
+            HttpPost objHttpPost = null;
+            String strUrluserTABLE = "http://swiftcodingthai.com/golf/php_get_data_golf.php";
+            String strUrldeviceTABLE = "http://swiftcodingthai.com/golf/php_get_data_device.php";
+
+            try {
+
+                HttpClient objHttpClient = new DefaultHttpClient();
+
+                switch (intTimes) {
+                    case 1:
+                        objHttpPost = new HttpPost(strUrluserTABLE);
+                        break;
+                    case 2:
+                        objHttpPost = new HttpPost(strUrldeviceTABLE);
+                        break;
+                }
+
+                HttpResponse objHttpResponse = objHttpClient.execute(objHttpPost);
+                HttpEntity objHttpEntity = objHttpResponse.getEntity();
+                objInputStream = objHttpEntity.getContent();
+
+            } catch (Exception e) {
+                Log.d("Suthep", "InputStream ==> " + e.toString());
+            }
+
+            //2. Create JSON String
+            try {
+
+                BufferedReader objBufferedReader = new BufferedReader(new InputStreamReader(objInputStream, "UTF-8"));
+                String strLine = null;
+                StringBuilder objStringBuilder = new StringBuilder();
+
+                while ((strLine = objBufferedReader.readLine()) != null) {
+                    objStringBuilder.append(strLine);
+                }
+
+                objInputStream.close();
+                strJSON = objStringBuilder.toString();
+
+
+            } catch (Exception e) {
+                Log.d("Suthep", "JSON String ==> " + e.toString());
+            }
+
+
+            //3. Update to SQLite
+            try {
+
+                JSONArray objJsonArray = new JSONArray(strJSON);
+
+                for (int i = 0; i < objJsonArray.length(); i++) {
+
+                    JSONObject object = objJsonArray.getJSONObject(i);
+
+                    switch (intTimes) {
+                        case 1:
+
+                            //For userTABLE
+                            String strUser = object.getString("User");
+                            String strPassword = object.getString("Password");
+                            String strOfficer = object.getString("Officer");
+                            String strPermission = object.getString("Permission");
+                            objManageTABLE.addUser(strUser, strPassword, strOfficer, strPermission);
+
+                            break;
+                        case 2:
+
+                            //For deviceTABLE
+                            String strTAGnfc = object.getString("tagNFC");
+                            String strName = object.getString("Name");
+                            String strLocation = object.getString("Location");
+                            String strStatus = object.getString("Status");
+                            String strDate = object.getString("Date");
+                            String strComment = object.getString("Comment");
+                            objManageTABLE.addDevice(strTAGnfc, strName, strLocation, strStatus, strDate, strComment);
+
+                            break;
+                    }
+
+                } //for
+
+            } catch (Exception e) {
+                Log.d("Suthep", "Update ==> " + e.toString());
+            }
+
+
+
+
+            intTimes += 1;
+        }   // while
 
 
     }   // synJSONtoSQLite
